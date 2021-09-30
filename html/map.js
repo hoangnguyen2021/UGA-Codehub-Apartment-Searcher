0445fd3613ad7ad793f70c5e979d53cdd69d8bd2
stephennemeth4 <sjn57152@uga.edu>
2021-09-30 17:19:07 -0400

    September 30th, 2021
    Added some stuff to the .css file

--- html/map.js
+++ html/map.js
@@ -1,15 +1,15 @@
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
-  const uluru = { lat: 33.9558, lng: -83.3773};
-  // The map, centered at Uluru
+  // The location of UGA
+  const UGA = { lat: 33.9558, lng: -83.3773};
+  // The map, centered at UGA
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 32,
-    center: uluru,
+    zoom: 14,
+    center: UGA,
   });
-  // The marker, positioned at Uluru
+  // The marker, positioned at UGA
   const marker = new google.maps.Marker({
-    position: uluru,
+    position: UGA,
     map: map,
   });
 }

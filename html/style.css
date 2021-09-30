ff5a4a84b849c3e261505c1b2515d489294d0b4c
Bret Jones <45405964+Iceman013@users.noreply.github.com>
2021-09-29 22:30:49 -0400

    Create style.css

---
+++ html/style.css
@@ -0,0 +1,27 @@
+<style>
+* {
+  box-sizing: border-box;
+}
+[class *= "box-"]{
+  float: left;
+  padding: 10px;
+  border: 2px solid powderblue;
+}
+.box-filter {
+  width: 30%;
+  background-color: rgb(255,0,0);
+}
+.box-list {
+  width: 30%;
+}
+.box-map {
+  width: 40%;
+}
+.box li {
+  list-style-type: none;
+  padding: 10px;
+  margin-bottom: 5px;
+  background-color: rgb(32,192,255);
+  color: rgb(255,255,255);
+  box-shadow: 0 2px 5px rgb(128,128,128);
+</style>

4722dafc716c5e51711c6105ef7e595f8a7b5d8c
stephennemeth4 <sjn57152@uga.edu>
2021-10-03 20:15:27 -0400

    Added showMarkers()
    added a javascript function in map.js that takes position markers as defined in the google maps api and displays them by passing them into the initMap() function as varargs

--- html/map.js
+++ html/map.js
@@ -1,19 +1,29 @@
 // Initialize and add the map
-function initMap() {
+function initMap(...positions) {
   // The location of UGA
   const UGA = { lat: 33.9558, lng: -83.3773};
   // The map, centered at UGA
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 14,
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
     center: UGA,
   });
   // The marker, positioned at UGA
-  const marker = new google.maps.Marker({
+  var marker = new google.maps.Marker({
     position: UGA,
     map: map,
   });
 
-  function showMarker() {
-
-  };
+  for (var position of positions) {
+    var newMarker = new google.maps.Marker({
+      position: position,
+    })
+    newMarker.setMap(map);
+  }
 }
+
+function showMarkers() {
+  var position1 = {lat: 33.9498, lng: -83.3734 }
+  var position2 = {lat: 33.9598, lng: -83.3744};
+  initMap(position1, position2);
+}
+

cf578ccad10c5215819961d0f229cf18ecd1ca86
Bret Jones <45405964+Iceman013@users.noreply.github.com>
2021-10-06 19:29:07 -0400

    Add meets condition

--- html/list.js
+++ html/list.js
@@ -1,16 +1,23 @@
 var apartments = [];
-apartments.push(new Apartment("a", "Worst Western", "www.here.com", "Big place", 300, 2, 3, false, true, false, false, {lat: 33.9598, lng: -83.370}, false));
-apartments.push(new Apartment("b", "La Keenta", "www.there.com", "Tiny place", 400, 1, 2, true, true, true, true, {lat: 33.9598, lng: -83.371}, false));
-apartments.push(new Apartment("c", "Holiday Out", "www.este.com", "Broken down", 350, 2, 3, true, false, false, true, {lat: 33.9598, lng: -83.372}, false));
-apartments.push(new Apartment("d", "Mary Ott", "www.coolmath.com", "Affordable", 476, 3, 2, true, true, false, true, {lat: 33.9598, lng: -83.373}, false));
-apartments.push(new Apartment("e", "Fake name", "www.website?.com", "For the rich", 100, 3, 3, false, false, true, true, {lat: 33.9598, lng: -83.374}, false));
-function Apartment(id, name, link, description, position, onMap) {
+apartments.push(new Apartment("a", "Worst Western", "www.here.com", "Big place", 300, 2, 3, false, true, false, {lat: 33.9598, lng: -83.370}, false, true));
+apartments.push(new Apartment("b", "La Keenta", "www.there.com", "Tiny place", 400, 1, 2, true, true, true, {lat: 33.9598, lng: -83.371}, false, true));
+apartments.push(new Apartment("c", "Holiday Out", "www.este.com", "Broken down", 350, 2, 3, true, false, false, {lat: 33.9598, lng: -83.372}, false, true));
+apartments.push(new Apartment("d", "Mary Ott", "www.coolmath.com", "Affordable", 476, 3, 2, true, true, false, {lat: 33.9598, lng: -83.373}, false, true));
+apartments.push(new Apartment("e", "Fake name", "www.website?.com", "For the rich", 100, 3, 3, false, false, true, {lat: 33.9598, lng: -83.374}, false, true));
+function Apartment(id, name, link, description, price, bed, bath, petFriendly, dorm, apartment, position, onMap, meets) {
   this.id = id;
   this.name = name;
   this.link = link;
   this.description = description;
+  this.price = price;
+  this.bed = bed;
+  this.bath = bath;
+  this.petFriendly = petFriendly;
+  this.dorm = dorm;
+  this.apartment = apartment;
   this.position = position;
   this.onMap = onMap;
+  this.meets = meets;
 }
 function loadList() {
   var a = 0;
@@ -20,25 +27,12 @@ function loadList() {
   }
 }
 function function1() {
-  var a = 0;
-  if (a == 0) {
-    displayApartment(false, 1);
-  }
-  if (a == 1) {
-    displayApartment(false, 2);
-  }
-  if (a == 2) {
-    displayApartment(true, 1);
-  }
-  a = a + 1;
+  apartments[0].meets = !apartments[0].meets;
+  displayApartment(0);
+  //var flist = showPreferences();
 }
 function showMap(input) {
   apartments[input].onMap = !apartments[input].onMap;
-  if (apartments[input].onMap) {
-    document.getElementById("checkbox" + apartments[input].id).classList.add("shown");
-  } else {
-    document.getElementById("checkbox" + apartments[input].id).classList.remove("shown");
-  }
   var a = 0;
   var mList = [];
   while (a < apartments.length) {
@@ -49,8 +43,8 @@ function showMap(input) {
   }
   initMap(mList);
 }
-function displayApartment(boolean, number) {
-  if (boolean) {
+function displayApartment(number) {
+  if (apartments[number].meets) {
     document.getElementById(apartments[number].id).style.display = "block";
   } else {
     document.getElementById(apartments[number].id).style.display = "none";
@@ -65,7 +59,6 @@ function createApartment(number) {
   divElement.classList.add("detail");
   
   item = document.createElement("button");
-  item.id = "checkbox" + apartments[number].id;
   item.classList.add("check");
   item.addEventListener("click", function() {
     this.classList.toggle("shown");

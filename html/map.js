bd435df3c707338d50059567cc281abc549fc04c
Bret Jones <45405964+Iceman013@users.noreply.github.com>
2021-10-12 22:39:05 -0400

    Improved calling abilities
    Made it so that each apartment contains the visible state used during map initialization.
    Made it so updating the map will not change the center.
    Gave markers titles and tooltips.

--- html/map.js
+++ html/map.js
@@ -1,26 +1,48 @@
 // Initialize and add the map
-function initMap(positions) {
-  // The location of UGA
-  const UGA = { lat: 33.9558, lng: -83.3773};
-  // The map, centered at UGA
+const UGA = {lat: 33.9558, lng: -83.3773};
+var center = UGA;
+function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 13,
-    center: UGA,
+    center: center,
   });
+  map.addListener("center_changed", () => {
+    center = map.getCenter();
+  })
   // The marker, positioned at UGA
-  var marker = new google.maps.Marker({
+  const marker = new google.maps.Marker({
     position: UGA,
-    map: map,
+    map,
+    title: "Home Sweet Home",
   });
-
-  for (var position of positions) {
-    var newMarker = new google.maps.Marker({
-      position: position,
-    })
-    newMarker.setMap(map);
+  const infowindow = new google.maps.InfoWindow({
+    content: "Home Sweet Home",
+  });
+  marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+    });
+  });
+  var a = 0;
+  while (a < apartments.length) {
+    const marker = new google.maps.Marker({
+      position: apartments[a].position,
+      map,
+      visible: apartments[a].onMap,
+    });
+    const infowindow = new google.maps.InfoWindow({
+      content: apartments[a].name,
+    });
+    marker.addListener("click", () => {
+      infowindow.open({
+        anchor: marker,
+        map,
+      });
+    });
+    a = a + 1;
   }
 }
-
 function showMarkers() {
   var position1 = {lat: 33.9498, lng: -83.3734 }
   var position2 = {lat: 33.9598, lng: -83.3744};
